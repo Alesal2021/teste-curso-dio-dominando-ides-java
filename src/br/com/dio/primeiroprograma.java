9c2c7e910067a1753b78af1fadcac858af774b45
Alesal2021 <alessandrocontato1975@gmail.com>
2021-12-12 23:13:00 -0300

    First commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		gato gato = new gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		/*int a = 3;
+		int b = 4;
+		System.out.println("Hello world " + (a+b));*/
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Livros {
+	private String nome;
+    private String npag;
+    
+}
